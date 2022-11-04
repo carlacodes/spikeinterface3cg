d3c23667b2b198b62871019cb147b065fb81f56b
zceccgr <zceccgr@login13.myriad.ucl.ac.uk>
2022-11-04 17:12:52 +0000

    separate test file for Eclair

--- interfacetest_jules_cg_kilosort_concatenate_ms4_eclair.py
+++ interfacetest_jules_cg_kilosort_concatenate_ms4_eclair.py
@@ -100,7 +100,7 @@ class TDTData:
         self.we.run_extract_waveforms(n_jobs=3, chunk_size=30000)
         print(self.we)
 
-        export_to_phy(self.we, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1901_Crumble//wpsoutput13',
+        export_to_phy(self.we, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1902_Eclair//wpsoutput13',
                       compute_pc_features=False, compute_amplitudes=True, copy_binary=True)
 
     def save_ks_as_phy(self):
@@ -111,7 +111,7 @@ class TDTData:
         self.we.run_extract_waveforms(n_jobs=3, chunk_size=30000)
         print(self.we)
 
-        export_to_phy(self.we, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1901_Crumble//wpsoutput13',
+        export_to_phy(self.we, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1902_Eclair//wpsoutput13',
                       compute_pc_features=False, compute_amplitudes=True, copy_binary=True)
 
 
@@ -172,7 +172,7 @@ def save_ks_as_phy_alone(data_test):
     # self.we.run_extract_waveforms(n_jobs=3, chunk_size=30000)
     # print(self.we)
 
-    export_to_phy(data_test, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1901_Crumble//wpsoutput13',
+    export_to_phy(data_test, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1902_Eclair//wpsoutput13',
                   compute_pc_features=False, compute_amplitudes=True, copy_binary=True)
 
             
@@ -183,32 +183,32 @@ def save_as_phy_alone(data_test, rec):
     data_test.we.set_params(ms_before=2., ms_after=2., max_spikes_per_unit=1000)
     data_test.we.run_extract_waveforms(n_jobs=3, chunk_size=30000)
     print(data_test.we)
-    export_to_phy(data_test.we, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1901_Crumble//wpsoutput29102022bb4bb5',
+    export_to_phy(data_test.we, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1902_Eclair//wpsoutput29102022bb4bb5',
                         compute_pc_features=False, compute_amplitudes=True, copy_binary=True)
-    export_report(data_test.we, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1901_Crumble//wpsoutput29102022bb4bb5_2')
+    export_report(data_test.we, '/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data//F1902_Eclair//wpsoutput29102022bb4bb5//report//')
 
 
 
 def main():
-    #/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data/F1901_Crumble
-    datadir = Path('/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data/F1901_Crumble')
+    #/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data/F1902_Eclair
+    datadir = Path('/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data/F1902_Eclair')
     logger.info('at main function')
     dp = datadir / 'BlockNellie-162'
     store = ['BB_4', 'BB_5']
     recording_list = []
 
     ##If using kilosort, this spike sorter is going to call the latest version of MATLAB irrespective of what you use normally for kilosort,
-    output_folder = Path('/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data/F1901_Crumble/wpsoutputBB4BB5')
+    output_folder = Path('/home/zceccgr/Scratch/zceccgr/Electrophysiological_Data/F1902_Eclair/wpsoutputBB4BB5')
     print('hello, concatenating neural data blocks now')
-    for i in range(1, 18): #range of intra trial roving stimuli for crumble
+    for i in range(31, 51): #range of intra trial roving stimuli for crumble
         print(i)
         block_ind = 'BlockNellie-' + str(i)
         print(block_ind)
         logger.info(f'variable: {block_ind}')
 
         dp2 = datadir / block_ind
-        if i == 6 or i==7 or i==2 or i==17:
-            continue
+        # if i == 6 or i==7 or i==2 or i==17:
+        #     continue
 
         if os.path.isdir(dp2):
             print(i)
